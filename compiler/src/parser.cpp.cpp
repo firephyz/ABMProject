7a6d136ebb60ee69fd10e40574a4c0fae3241774
princeKhale <loganjblan2@gmail.com>
2019-03-13 18:34:54 -0400

    Added parser rough draft

---
+++ compiler/src/parser.cpp.cpp
@@ -0,0 +1,113 @@
+/*
+ * GAMP PARSER 
+ * 3.9.19
+ */
+
+using namespace std;
+#include <libxml2/libxml/parser.h>
+#include <istream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+
+void readInputFile(const char* fileName);
+void parseEnviroment(xmlNodePtr envChild);
+void parseAgents(xmlNodePtr agentsChild);
+xmlAttrPtr xmlGetAttribute(xmlNodePtr node, const char * attr_name);
+bool stobool(std::string str);
+ 
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "Improper use of parser, requires ./parser "
+                  << "[filename] [options]" << std::endl;
+        return(-1);
+    }
+
+    char* fileName = argv[1];
+    std::cout << "Input File: " << fileName << std::endl;
+    
+    readInputFile(fileName);
+    return(0);
+}
+
+void readInputFile(const char* fileName) {
+    xmlDocPtr inputDoc = xmlReadFile(fileName, NULL, 0x0);
+    if (inputDoc == NULL) {
+        std::cerr << "Couldn't read input file! \'" 
+                  << fileName << "\'." << std::endl;
+        exit(-1);
+    }
+    std::cout << "File loaded in!" << std::endl; 
+  
+    xmlNodePtr root = xmlDocGetRootElement(inputDoc);
+    xmlNodePtr child = xmlFirstElementChild(root);   
+
+    while (child != NULL) {	
+        if (xmlStrcmp(child->name, (const xmlChar*)"Enviroment") == 0){ 
+   		parseEnviroment(child);
+        } else if(xmlStrcmp(child->name, (const xmlChar*)"AgentDefinitions") == 0){ 
+   		parseAgents(child);
+        } else {
+  		std::cout << "OOPS" << std::endl;
+         child = xmlNextElementSibling(child);
+   	}
+    }
+    
+  
+
+    /* Simple Test 
+    while (child != NULL) {
+        std::cout << child->name << std::endl; 
+        child = xmlNextElementSibling(child);
+    }
+    */
+}
+
+
+void parseEnviroment(xmlNodePtr envChild) { 
+        std::string relationType = (const char*)xmlGetAttribute(envChild, (const char*)"relationType")->children->content;
+        xmlNodePtr curNode = NULL;    		
+   	int numOfDim = 0;
+	bool wrap = 0;
+
+	if (relationType != NULL) { 
+    		if(relationType == "spatial") {
+		        curNode = xmlFirstElementChild(envChild);
+			if (xmlStrcmp(curNode->name, (const xmlChar*)"spatialRelation") == 0){
+                        	numOfDim = std::stoi((const char*)xmlGetAttribute(curNode, "dimensions")->children->content, NULL, 10);
+				wrap = stobool((const char*)xmlGetAttribute(curNode, "wrap")->children->content);	
+				std::cout << numOfDim << "\n" << wrap << std::endl;  
+			} else {
+				std::cout << "Invalid Enviroment Definiton" << std::endl;
+        		}
+		} 
+	} 
+}
+
+void parseAgents(xmlNodePtr agentsChild) { 
+
+}
+
+xmlAttrPtr xmlGetAttribute(xmlNodePtr node, const char * attr_name) {
+  xmlAttrPtr result = node->properties;
+  while(result != NULL) {
+    if(xmlStrcmp(result->name, (const xmlChar *)attr_name) == 0) {
+      return result;
+    }
+    result = result->next;
+  }
+  return (xmlAttrPtr)NULL;
+}
+
+bool stobool(std::string str) {
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::istringstream is(str);
+    bool b;
+    is >> std::boolalpha >> b;
+    return b;
+}
