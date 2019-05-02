c67b1372738a4c7c2301f1c3169b182b91cee438
alex <leeab2@miamioh.edu>
2019-05-01 20:53:38 -0700

    fork for merging env

---
+++ compiler/cmake-build-debug/CMakeFiles/gamp.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/abmodel.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/abmodel.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/agent_form.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/agent_form.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/comms.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/comms.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/compiler_types.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/compiler_types.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/initial_agent.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/initial_agent.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/main.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/main.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/parser.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/parser.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/source_ast.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/source_ast.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/source_c.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/source_c.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/source_verilog.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/source_verilog.cpp.o"
+  "/home/alex/Documents/449_final/ABMProject/compiler/src/util.cpp" "/home/alex/Documents/449_final/ABMProject/compiler/cmake-build-debug/CMakeFiles/gamp.dir/src/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ compiler/cmake-build-debug/CMakeFiles/gamp.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamp.dir/src/abmodel.cpp.o"
+  "CMakeFiles/gamp.dir/src/agent_form.cpp.o"
+  "CMakeFiles/gamp.dir/src/comms.cpp.o"
+  "CMakeFiles/gamp.dir/src/compiler_types.cpp.o"
+  "CMakeFiles/gamp.dir/src/initial_agent.cpp.o"
+  "CMakeFiles/gamp.dir/src/main.cpp.o"
+  "CMakeFiles/gamp.dir/src/parser.cpp.o"
+  "CMakeFiles/gamp.dir/src/source_ast.cpp.o"
+  "CMakeFiles/gamp.dir/src/source_c.cpp.o"
+  "CMakeFiles/gamp.dir/src/source_verilog.cpp.o"
+  "CMakeFiles/gamp.dir/src/util.cpp.o"
+  "gamp.pdb"
+  "gamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
